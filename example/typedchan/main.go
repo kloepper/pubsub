c69c6a67d6b4c30ac7b14c9f4ce257f934e528bf
Kyle Kloepper <kyle@defend7.com>
2016-02-11 12:15:12 -0800

    Rework example/typedchan example using myelin.

--- example/typedchan/main.go
+++ example/typedchan/main.go
@@ -2,82 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 
-	"github.com/benburkert/pubsub"
+	"myelin"
 )
 
 type foo struct {
 	N int
 }
 
-type fooChan chan foo
-
-func (ch fooChan) PublishTo(ctx *pubsub.Context) error {
-	go func() {
-		defer ctx.Close()
-
-		for {
-			select {
-			case v, ok := <-ch:
-				if !ok {
-					return
-				}
-
-				ctx.Buffer.Write(v)
-			case <-ctx.Done:
-				return
-			}
-		}
-	}()
-
-	return nil
-}
-
-func (ch fooChan) SubscribeTo(ctx *pubsub.Context) error {
-	rfn := func(v interface{}) bool {
-		if v == ctx.Done {
-			close(ch)
-			ctx.Close()
-			return false
-		}
-		ch <- v.(foo)
-		return true
-	}
-
-	ctx.Buffer.ReadTo(rfn)
-	return nil
-}
-
 func main() {
-	ps, _ := pubsub.New(16, 4)
-
-	pch := make(fooChan)
-
-	if err := ps.AddPublisher(pch); err != nil {
-		panic(err)
-	}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(4)
-
-	for _, id := range []string{"A", "B", "C", "D"} {
-		ch := make(fooChan, 4)
-		ps.AddSubscriber(ch)
-
-		go func(ch <-chan foo, id string) {
-			defer wg.Done()
-
-			for v := range ch {
-				fmt.Printf("%s got foo.N=%d\n", id, v.N)
+	names := []string{"A", "B", "C", "D"}
+	for _, name := range names {
+		messages := make(chan foo)
+		myelin.Subscribe(messages)
+		printFormat := name + " got foo.N=%d\n"
+		go func() {
+			for {
+				fmt.Printf(printFormat, (<-messages).N)
 			}
-		}(ch, id)
+		}()
 	}
 
 	for i := 0; i <= 25; i++ {
-		pch <- foo{N: i}
+		myelin.Publish(foo{N: i})
 	}
-	ps.Close()
 
-	wg.Wait()
+	time.Sleep(time.Millisecond)
 }
